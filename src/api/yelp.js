df746caba9ca0dea3de5800428411a7df470129a
cluster05 <48400669+cluster05@users.noreply.github.com>
2021-05-20 19:47:58 +0530

    security pathc

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses",
     headers: {
-        Authorization: 'Bearer RSKod-yQVCeO2GJhjencoFPMAjTd1Okaw8ht2KI8VU4GvX7a2ghY7pAZENq6zcd1WGGz696-V-5NDOeCCQ7PnG1pu_ITuUX94SEep9sEYOV98V5o_ZsdOrGYxu2lYHYx'
+        Authorization: 'Bearer your-token-from-yelp'
     }
 });
